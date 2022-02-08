995a472482e31b90d119991c31692ad428fadfb0
Sergey <itsib.su@gmail.com>
2022-02-08 13:42:23 +0700

    first commit

---
+++ contracts/interfaces/IGasStationRecipient.sol
@@ -0,0 +1,28 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+pragma experimental ABIEncoderV2;
+
+interface IGasStationRecipient {
+
+    function isGasStation(address addressToCheck) public virtual view returns(bool);
+
+    function versionRecipient() external virtual view returns (string memory);
+
+    /**
+     * return the sender of this call.
+     * if the call came through our trusted forwarder, then the real sender is appended as the last 20 bytes
+     * of the msg.data.
+     * otherwise, return `msg.sender`
+     * should be used in the contract anywhere instead of msg.sender
+     */
+    function _msgSender() internal virtual view returns (address);
+
+    /**
+     * return the msg.data of this call.
+     * if the call came through our trusted forwarder, then the real sender was appended as the last 20 bytes
+     * of the msg.data - so this method will strip those 20 bytes off.
+     * otherwise (if the call was made directly and not through the forwarder), return `msg.data`
+     * should be used in the contract instead of msg.data, where this difference matters.
+     */
+    function _msgData() internal virtual view returns (bytes calldata);
+}
